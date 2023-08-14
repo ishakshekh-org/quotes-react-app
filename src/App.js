06f04521a5e9b10661f0705cc3169e54693a8eca
ishakshekh-org <shekhishak90@gmail.com>
2023-08-15 00:27:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    advice: "",
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+
+    return (
+      <div className="app">
+        <div className="card">
+          <div className="cardContainer">
+            <h1 className="heading"> {advice}</h1>
+          </div>
+
+          <button className="button" onClick={this.fetchAdvice}>
+            <span> Give Me Advice</span>
+          </button>
+        </div>
+
+        {/* gradient and blob */}
+        <div className=" gradient-1"></div>
+        <div className="gradient-2"></div>
+        <div className="blob"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
